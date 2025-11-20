baff265fd6dfc01843bbec4c6fae8aa7f5cded36
chethu08050 <chethankumarhc2@gmail.com>
2025-11-20 16:14:23 +0530

    Update VenueSection.jsx

--- src/VenueSection.jsx
+++ src/VenueSection.jsx
@@ -37,7 +37,8 @@ export default function VenueSection({
   }, []);
 
   const openInMaps = () => {
-    window.open(mapEmbedUrl.replace('embed?', 'search/?api=1&query=').split('!')[0], '_blank');
+    const cleanUrl = mapEmbedUrl.replace('/embed?', '/?').split('!')[0];
+    window.open(cleanUrl, '_blank');
   };
 
   return (
@@ -103,6 +104,13 @@ export default function VenueSection({
               frameBorder="0"
             />
             <div className="venue-map-overlay" />
+            <button className="venue-map-cta" onClick={openInMaps}>
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"/>
+                <circle cx="12" cy="10" r="3"/>
+              </svg>
+              Open in Maps
+            </button>
           </div>
         </div>
 
