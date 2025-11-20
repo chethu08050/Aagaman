97a4384e7f1bb7d1a3154c03afa13fd6a40438de
chethu08050 <chethankumarhc2@gmail.com>
2025-11-20 16:56:35 +0530

    Delete LoadingFallback.jsx

--- src/LoadingFallback.jsx
+++
@@ -1,9 +0,0 @@
-import './LoadingFallback.css';
-
-export default function LoadingFallback({ minHeight = '100vh' }) {
-  return (
-    <div className="loading-fallback" style={{ minHeight }}>
-      <div className="loading-spinner"></div>
-    </div>
-  );
-}
